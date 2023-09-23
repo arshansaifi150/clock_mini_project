da3adac9bd66c82cd590299f02a18f46d4fa3c44
Mohd Arshan <110154295+arshansaifi150@users.noreply.github.com>
2023-09-23 16:57:06 +0000

    date_and_time done

--- Clock_Project/index.js
+++ Clock_Project/index.js
@@ -1,12 +1,11 @@
-let time = document.querySelector('.time');
-let date = document.querySelector('.data')
-
-
-setInterval(function(){
-    const clockTime = new Date();    
-    time.innerHTML = clockTime.toLocaleTimeString() 
-},1000)
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
 
 const clockDate = new Date();
-date.innerHTML  = clockDate.toLocaleDateString();
+date.innerHTML = clockDate.toLocaleDateString()
 
+setInterval(function (){
+    const clockTime = new Date();
+    time.innerHTML = clockTime.toLocaleTimeString()
+     
+},1000);
